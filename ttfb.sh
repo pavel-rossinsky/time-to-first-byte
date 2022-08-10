7d3369b304ab65693718645c141f6dc776f4b80a
Pavel Rossinsky <po.rossinsky@gmail.com>
2022-08-10 09:58:34 +0200

    Add a new -r option that allows to read random urls from the file

--- ttfb.sh
+++ ttfb.sh
@@ -10,6 +10,7 @@ function help() {
     echo -e "-u \t Single URL. Overwrites the -f option."
     echo -e "-f \t Path to the file with URLs."
     echo -e "-l \t Limit of the URLs to read from the file."
+    echo -e "-r \t Reads random rows from the file."
     echo -e "-a \t Overwrites the default user-agent."
     echo -e "-i \t [Flag] Attempt to invalidate cache by adding a timestamp to the URLs."
     echo -e "-h \t [Flag] Help."
@@ -21,13 +22,14 @@ if [[ ! $* =~ ^\-.+ ]]; then
     help
 fi
 
-while getopts "f:u:l:a:ih" opt; do
+while getopts "f:u:l:a::rih" opt; do
     case "$opt" in
     f) file=${OPTARG} ;;
     u) url=${OPTARG} ;;
     a) user_agent="${OPTARG}" ;;
     l) limit=${OPTARG} ;;
     i) invalidate_cache=1 ;;
+    r) random=1 ;;
     h) help ;;
     *) help ;;
     esac
@@ -39,6 +41,10 @@ fi
 
 if [[ -z ${invalidate_cache+set} ]]; then
     invalidate_cache=0
+fi
+
+if [[ -z ${random+set} ]]; then
+    random=0
 fi
 
 function send_request() {
@@ -81,32 +87,61 @@ if [[ ! -f $file ]]; then
 fi
 
 total_time=0
-counter=0
+visited_counter=0
 non_200_counter=0
+
 printf "\n"
-while read -r in || [ -n "$in" ]; do
-    counter=$((counter + 1))
 
-    in="$(prepare_url "$in" $invalidate_cache)"
+function visit_url() {
+    ((visited_counter+=1))
 
-    read -r curr_time http_code <<<"$(send_request "$in" "$user_agent")"
+    url="$(prepare_url "$1" $invalidate_cache)"
+
+    read -r curr_time http_code <<<"$(send_request "$url" "$user_agent")"
     if [[ $http_code != '200' ]]; then
         non_200_counter=$((non_200_counter + 1))
-        echo $counter "$http_code" "$in" -
+        echo "$visited_counter" "$http_code" "$url" -
     else
         total_time=$(awk "BEGIN {print $total_time+$curr_time; exit}")
-        average=$(awk "BEGIN {print $total_time/($counter-$non_200_counter); exit}")
-        echo $counter "$http_code" "$in" "$curr_time" "$total_time" "$average"
+        average=$(awk "BEGIN {print $total_time/($visited_counter-$non_200_counter); exit}")
+        echo "$visited_counter" "$http_code" "$url" "$curr_time" "$total_time" "$average"
     fi
+}
 
-    if [[ $limit -gt 0 && $counter -ge $limit ]]; then
-        break
-    fi
-done <"$file"
+if [[ $random == 1 ]]; then
+    random_rows=()
+    while IFS= read -r row ; do random_rows+=("$row"); done <<< "$(
+    awk -v loop=$limit -v range="$(wc -l "$file")" 'BEGIN {
+            srand()
+            do {
+                numb = 1 + int(rand() * range)
+                if (!(numb in prev)) {
+                   print numb
+                   prev[numb] = 1
+                   count++
+                }
+            } while (count<loop)
+        }'
+    )"
+
+    for row in "${random_rows[@]}"
+    do
+        in=$(awk "NR==$row{ print; exit }" "$file")
+        visit_url "$in"
+    done
+else
+    while read -r in || [ -n "$in" ]; do
+        visit_url "$in"
+
+        if [[ $limit -gt 0 && $visited_counter -ge $limit ]]; then
+            break
+        fi
+    done <"$file"
+fi
 
 printf "\n"
-echo "Pages visited:       $((counter))"
-echo "Pages evaluated:     $((counter - non_200_counter))"
+echo "Pages visited:       $((visited_counter))"
+echo "Pages evaluated:     $((visited_counter - non_200_counter))"
 echo "Pages skipped:       $non_200_counter"
 echo "Total time elapsed:  $total_time (s)"
-echo "Average TTFB:        $(awk "BEGIN {print $total_time/($counter-$non_200_counter); exit}") (s)"
+echo "Average TTFB:        $(awk "BEGIN {print $total_time/($visited_counter-$non_200_counter); exit}") (s)"
